69094164685b4aa72613236fe202545380385b7c
Kaz <hexter1994@gmail.com>
2022-07-16 23:57:55 +0900

    Initial Commit

---
+++ gitHubTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitHubTest
+//
+//  Created by 원태영 on 2022/07/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
